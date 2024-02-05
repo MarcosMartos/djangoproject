6f1f40320a8e4a656e491b6f79ea2500371d1aac
Marcos Martos <marcosmartostuvi@gmail.com>
2024-02-05 11:34:20 -0300

    templates

--- myapp/views.py
+++ myapp/views.py
@@ -5,16 +5,25 @@ from django.shortcuts import get_object_or_404, render
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    title = "Django Course"
+    return render(request, "index.html", {
+        "title": title
+    })
 
 def about(request):
-    return render(request, "about.html")
+    username = "Marcos Martos"
+    return render(request, "about.html", {
+        "username": username
+    })
 
 def hello(request, username):
     return HttpResponse("<h1>Hello %s </h1>" % username)
     
 def projects(request):
-    return render(request, "projects.html")
+    projects = list(Project.objects.all())
+    return render(request, "projects.html", {
+        "Projects": projects
+    })
 
 def tasks(request):
     # task = get_object_or_404(Task, id=id)
